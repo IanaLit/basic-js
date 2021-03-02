395f211fdc14700cbb1c8b860b17f623d3231ccc
iana <litvinova.iana1212@gmail.com>
2021-03-03 00:11:27 +0700

    feat: add repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str,  options) {
+  
+  class RepeatString {
+   
+   constructor(options){
+   this.repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+   this.addition = (typeof options.addition === "boolean" || options.addition === "null") ? options.addition.toString() : options.addition;
+   this.additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+   this.separator = options.separator ? options.separator : '+';
+   this.additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
+   this.answer = ""; 
+    }
+    
+   }
+ let repeate = function(str, options)
+   {
+     
+     let result = new RepeatString(options);
+     for(let i=0; i< result.repeatTimes; i++) {
+       result.answer += str;
+       if(result.addition || result.addition === null){
+         result.answer += result.addition;
+         let j = result.additionRepeatTimes;
+         while(j !== 1){
+           j--;
+           result.answer += result.additionSeparator+ result.addition;
+         }
+       }
+       if(i !== result.repeatTimes-1)
+       result.answer+=result.separator;
+     }
+     return result.answer;
+   }
+ 
+   return repeate(str, options);
+ };
   
