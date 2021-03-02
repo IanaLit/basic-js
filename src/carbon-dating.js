ccdcff81ca3c2dd60fd6886fed0f66e43da28376
iana <litvinova.iana1212@gmail.com>
2021-03-02 15:55:18 +0700

    feat: add dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(!sampleActivity || 
+    typeof sampleActivity !== 'string' || 
+    parseFloat(sampleActivity) <= 0 || 
+    parseFloat(sampleActivity) >= 15 || 
+    isNaN(sampleActivity) ||
+    !Number.isFinite(parseFloat(sampleActivity))){
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/(Math.LN2/ HALF_LIFE_PERIOD));
+  
 };
