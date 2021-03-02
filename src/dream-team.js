a97f9628c814b7882aae203185872c99281adf8f
iana <litvinova.iana1212@gmail.com>
2021-03-02 16:43:21 +0700

    feat: add createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(members instanceof Array){
+    return members.map(member => {
+
+     if (typeof member === "string") return member.toUpperCase().trim().split('').slice(0, 1);
+     
+  }).sort().join('');
+  }
+  else return false;
+
 };
