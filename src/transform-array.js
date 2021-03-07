111fed9755f9d401c153329248d17982c14524dc
iana <litvinova.iana1212@gmail.com>
2021-03-07 13:06:14 +0700

    feat:  add transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports =  function transform(arr) {
+let tmp =[]; 
+
+  for (let i=0; i< arr.length; i++){
+
+    if (arr[i] === '--discard-next') continue;
+    else if (arr[i-1] === '--discard-next') continue;
+    else if(arr[i] === '--discard-prev') continue; 
+    else if(arr[i+1] === '--discard-prev') continue;
+
+    else if (arr[i] === '--double-next'){
+      if (i === arr.length-1) continue;
+      else if(arr[i+1] !== undefined ) tmp.push(arr[i+1]);  
+    } 
+    else if (arr[i-1] === '--double-next' && arr[i] !== undefined) tmp.push(arr[i]);
+    else if( arr[i] === '--double-prev'){
+      if(arr[i-2] !== '--discard-next' && arr[i-1] !== undefined) tmp.push(arr[i-1]);
+      else continue;
+    } else if(arr[i+1] === '-double-prev'){
+      if(arr[i-1] !== '--discard-next' && arr[i] !== undefined) tmp.push(arr[i]);
+      else continue;
+    }
+      else tmp.push(arr[i]);
+  }
+  return tmp;
+}
