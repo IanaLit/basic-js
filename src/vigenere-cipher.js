a163f906d29aa8f502f7b3a5ce1775c6be502713
iana <litvinova.iana1212@gmail.com>
2021-03-09 20:10:49 +0700

    feat: create VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,67 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(straight) {
+   this.straight =  straight === undefined ? true : false;
+  }
+  encrypt(str, key) {
+    if(str === undefined || key === undefined) throw new Error();
+    let keyArray =[];  
+    keyArray = key.toUpperCase().split('');
+    let add = str.length - key.length; 
+    while(keyArray.length < str.length){
+      keyArray = str.length >= add ?  
+      keyArray.concat(keyArray.slice(0, add)) : 
+      keyArray.concat(keyArray.slice(0, add - keyArray.length)); 
+    }
+    str = str.toUpperCase().split('');
+    let keyIndex = 0;
+    let result =[];
+
+    result= str.map((letter, index) => {
+        if(str[index].match(/[A-Z]/)){
+        let res = String.fromCharCode(Number(((str[index]?.charCodeAt(0) - 65 + keyArray[keyIndex]?.charCodeAt(0)-65) % 26) + 65));
+        keyIndex++;
+        return res;
+      }
+      else {
+        return letter;
+      }
+       
+    });
+    
+    return this.straight ? result.join('') : result.reverse().join('');
+  } 
+  decrypt(cipher, key) {
+    if(cipher=== undefined || key === undefined) throw new  Error();
+    let keyArray =[];  
+    keyArray = key.toUpperCase().split('');
+    let add = cipher.length - key.length; 
+    while(keyArray.length < cipher.length){
+      keyArray = cipher.length >= add ?  
+      keyArray.concat(keyArray.slice(0, add)) : 
+      keyArray.concat(keyArray.slice(0, add - keyArray.length)); 
+    }
+    cipher = cipher.toUpperCase().split('');
+    let keyIndex = 0;
+    let result =[];
+    result = cipher.map((letter, index) => {
+      
+      if(cipher[index].match(/[A-Z]/)){
+        let res = String.fromCharCode(Number(((cipher[index].charCodeAt(0) - 65 + 26 - (keyArray[keyIndex].charCodeAt(0)-65)) % 26) + 65));
+        
+        keyIndex++;
+        return res;
+      }
+      else {
+        
+        return letter;
+      }
+       
+    });
+    return this.straight ? result.join('') : result.reverse().join('');
+     
   }
 }
 
